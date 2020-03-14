965b31f24e378887e334704fb7b3cc7cbaf5e232
Tim Caswell <tim@creationix.com>
2020-03-14 16:59:38 -0500

    First working version!!

--- src/parse.js
+++ src/parse.js
@@ -2,161 +2,247 @@ const { inspect } = require('util')
 const { readFileSync } = require('fs')
 const { parse: schemaParse } = require('ipld-schema')
 
-const dump = value => inspect(value, { depth: 0, colors: true })
+const dump = value => inspect(value, { depth: 100, colors: true })
 const helperMethods = {
-  expectedOneOf (value, types) {
+  expectedOneOf(value, types) {
     throw new TypeError(`${dump(this.constructor)}: Expected one of (${types.map(dump).join(', ')}), but found ${dump(value)}`)
   },
-  expected (value, type) {
+  expected(value, type) {
     throw new TypeError(`${dump(this.constructor)}: Expected ${dump(type)}, but found ${dump(value)}`)
   }
 }
 
+const longest = (a, b) => b.length - a.length
+
 const schema = schemaParse(readFileSync(`${__dirname}/selectors.ipldsch`, 'utf8'))
-const {
-  SelectorEnvelope,
-  Selector,
-  ExploreAll,
-  ExploreFields,
-  ExploreIndex,
-  ExploreRange,
-  ExploreRecursive,
-  RecursionLimit,
-  RecursionLimit_None,
-  RecursionLimit_Depth,
-  ExploreRecursiveEdge,
-  ExploreUnion,
-  ExploreConditional,
-  Matcher,
-  Condition
-} = helperGen(schema)
 
-function helperGen ({ types }) {
-  const helpers = {}
-
-  function isType (value, type) {
-    console.log({ value, type })
-    if (typeof type === 'string') {
-      if (type === 'String') {
-        return typeof value === 'string'
-      }
-      const Helper = helpers[type]
-      if (Helper) {
-        return value instanceof Helper
-      }
-      console.error({ value, type })
-      throw new Error('TODO: implement more type checking')
-    }
-    if (type.kind === 'map') {
-      if (!value || value.constructor !== Object) {
-        return false
-      }
-      for (const key in value) {
-        if (!isType(key, type.keyType) || !isType(value[key], type.valueType)) return false
-      }
-      return true
-    }
+function parseString(input, offset) {
+  if (input[offset] !== "'") return;
+  const parts = [];
+  while (input[offset] === "'") {
+    const match = input.substr(offset).match(/'([^']*)'/)
+    if (!match) throw new Error(`Missing closing quote in string at ${offset}`)
+    parts.push(match[1]);
+    offset += match[0].length;
   }
-
-  for (const name in types) {
-    const entry = types[name]
-    const Helper = helpers[name] = function (arg) {
-      if (!(this instanceof Helper)) return new Helper(arg)
-
-      // Auto detect correct arm of keyed union.
-      if (entry.kind === 'union') {
-        if (entry.representation.keyed) {
-          let match
-          for (const key in entry.representation.keyed) {
-            if (isType(arg, entry.representation.keyed[key])) {
-              match = key
-              break
-            }
-          }
-          if (!match) {
-            console.error({ name, entry })
-            return this.expectedOneOf(arg, Object.values(entry.representation.keyed))
-          }
-          this[match] = arg
-        } else {
-          console.error({ name, arg, entry })
-          throw new Error('TODO: Implement other union representations')
-        }
-      } else if (entry.kind === 'struct') {
-        const matches = {}
-        for (const key in entry.fields) {
-          const { type } = entry.fields[key]
-          if (key in arg && isType(arg[key], type)) {
-            matches[key] = arg[key]
-            continue
-          }
-        }
-        if (entry.representation.map) {
-          for (const key in entry.representation.map.fields) {
-            const repr = entry.representation.map.fields[key]
-            const match = matches[key]
-            if (match === undefined) {
-              console.log({ name, arg, entry })
-              if (!entry.fields[key].optional) {
-                throw new TypeError(`Missing required struct field: ${name}.${key}`)
-              }
-            }
-            this[repr.rename || key] = match
-          }
-        } else {
-          console.error({ name, arg, entry })
-          throw new Error('TODO: Implement other struct representations')
-        }
-      } else if (entry.kind === 'int') {
-        if (!Number.isInteger(arg)) {
-          console.error({ name, arg, entry })
-          throw new TypeError('Expected integer')
-        }
-        this.toJSON = () => arg
-      } else {
-        console.error({ name, arg, entry })
-        throw new Error('TODO: Implement new kind')
-      }
-    }
-    Object.defineProperty(Helper, 'name', { value: name })
-    Object.setPrototypeOf(Helper.prototype, helperMethods)
-  }
-  return helpers
+  return [parts.join("'"), offset]
 }
 
-// // const sel = SelectorEnvelope(
-// //   Selector(ExploreRecursive({
-// //     limit: RecursionLimit(RecursionLimit_None()),
-// //     sequence: Selector(ExploreAll({
-// //       next:
-// //         Selector(ExploreRecursiveEdge())
-// //     }))
-// //   }))
-// // )
+function parseInteger(input, offset) {
+  let sign = 1;
+  if (input[offset] === '-') {
+    sign = -1;
+    offset++;
+  }
+  let byte = input.charCodeAt(offset)
+  if (byte < 0x30 || byte > 0x39) return
+  let val = 0;
+  while (byte >= 0x30 && byte <= 0x39) {
+    val = val * 10 + (byte - 0x30)
+    offset++
+    byte = input.charCodeAt(offset)
+  }
+  return [val * sign, offset]
+}
 
-// const sel = SelectorEnvelope(
-//   Selector(ExploreRecursive({
-//     limit: RecursionLimit(RecursionLimit_Depth(5)),
-//     sequence: Selector(ExploreFields({
-//       fields: {
-//         tree: Selector(ExploreRecursive({
-//           limit: RecursionLimit(RecursionLimit_None()),
-//           sequence: Selector(ExploreAll({
-//             next: Selector(ExploreRecursiveEdge())
-//           }))
-//         })),
-//         parents: Selector(ExploreAll({
-//           next: Selector(ExploreRecursiveEdge())
-//         }))
-//       }
-//     }))
-//   }))
-// )
+function parenCheck(input, offset, parse) {
+  let paren = input[offset] === '(' ? 1 : 0
+  const result = parse(input, offset + paren);
+  if (!result) return;
+  const [value, newOffset] = result;
+  // console.log({ value, paren, newOffset })
+  if (paren) {
+    if (input[newOffset] !== ')') throw new SyntaxError(`Missing closing parenthesis at ${newOffset}`)
+  }
+  return [value, newOffset + paren]
+}
 
-// console.log(inspect(sel, { depth: null, colors: true }))
-// console.log(inspect(JSON.parse(JSON.stringify(sel)), { depth: null, colors: true }))
+function emptyCheck(input, offset, parse) {
+  const result = parse(input, offset);
+  if (!result || result[1] === offset) return;
+  return result
+}
 
-function parse (rawInput) {
+function makeParser({ types }, entry, aliases = {}, ignores = []) {
+
+  let parsers = new Map();
+
+  for (const key of ignores) {
+    // console.log("Ignoring type", key)
+    parsers.set(key, (input, offset) => {
+      // console.warn("Ignoring type", key)
+    });
+  }
+
+  return gen(entry);
+
+  function gen(type) {
+    const existing = parsers.get(type);
+    if (existing) return existing;
+    const parser = genReal(type);
+    parsers.set(type, parser)
+    Object.defineProperty(parser, 'name', { value: type })
+    return parser;
+  }
+
+  function genReal(typeName) {
+    const type = types[typeName] || typeName;
+    if (type === typeName) typeName = undefined
+    if (type) {
+      if (type === 'String') return parseString;
+      if (type.kind === 'int') return parseInteger;
+      if (type.kind === 'map') return genMap(type);
+      if (type.kind === 'union' && type.representation && type.representation.keyed) return genKeyedUnion(typeName, type)
+      if (type.kind === 'struct' && type.fields && type.representation && type.representation.map) return genMappedStruct(typeName, type)
+    }
+    return (input, offset) => {
+      console.log({ typeName, type });
+      throw new Error("TODO: Support new type")
+    }
+  }
+
+  function genKeyedUnion(name, { representation: { keyed: keys } }) {
+    // console.log('genKeyedUnion', name)
+
+    const keywords = [];
+    const reverse = {};
+    for (const [key, type] of Object.entries(keys)) {
+      const list = aliases[type];
+      if (!list) continue;
+      // console.log({ key, list })
+      for (const alias of list) {
+        keywords.push(alias);
+        reverse[alias] = key;
+      }
+    }
+    keywords.sort(longest)
+
+    return (input, offset) => {
+      // console.log("parsing keyed union", name)
+      // console.log({ keywords, keys })
+
+      // console.log({ keywords, reverse });
+      for (const keyword of keywords) {
+        if (startsWith(input, offset, keyword)) {
+          const key = reverse[keyword];
+          const type = keys[key]
+          const result = parenCheck(input, offset + keyword.length, gen(type))
+          if (!result) throw new SyntaxError(`Failed parsing ${type} at ${offset}`)
+          const [value, newOffset] = result
+          return [{ [key]: value }, newOffset]
+        }
+      }
+      for (const [key, type] of Object.entries(keys)) {
+        if (aliases[type]) continue;
+        // console.log(name, key)
+        const result = emptyCheck(input, offset, gen(type))
+        if (!result) continue;
+        const [value, newOffset] = result
+        return [{ [key]: value }, newOffset]
+      }
+    }
+  }
+
+  function genMappedStruct(name, { fields, representation: { map } }) {
+    // console.log('genMappedStruct', name)
+    const fieldNames = Object.keys(fields).sort(longest)
+    const names = fieldNames.map(name => `${name}=`)
+    // console.log({ names, fieldNames })
+    return (input, offset) => {
+      // console.log("parsing mapped struct", name)
+      const struct = {};
+      // console.log(name, fields, map)
+      let start
+      outer: do {
+        start = offset;
+        // console.log({ offset, rest: input.substr(offset) })
+        for (let i = 0, l = names.length; i < l; i++) {
+          const name = names[i];
+          if (startsWith(input, offset, name)) {
+            const fieldName = fieldNames[i];
+            const { type } = fields[fieldName];
+            const result = emptyCheck(input, offset + name.length, gen(type));
+            if (!result) throw new SyntaxError(`Failed to parse named parameter at ${offset}`)
+            const [value, newOffset] = result;
+            // console.log("Named struct field", { fieldName, value })
+            struct[fieldName] = value
+            offset = newOffset
+            continue outer;
+          }
+        }
+        for (const [key, { type }] of Object.entries(fields)) {
+          const result = emptyCheck(input, offset, gen(type))
+          if (!result) continue;
+          const [value, newOffset] = result;
+          // console.log("Typed struct field", { key, value })
+          struct[key] = value;
+          offset = newOffset
+          continue outer
+        }
+      } while (offset > start)
+
+      const remapped = {}
+      for (const key of Object.keys(fields)) {
+        if (key in struct) {
+          const renamedKey = map.fields && map.fields[key] && map.fields[key].rename || key
+          remapped[renamedKey] = struct[key]
+        } else {
+          if (!fields[key].optional) {
+            throw new SyntaxError(`Missing required struct field "${key}" at ${offset}`)
+          }
+        }
+      }
+      return [remapped, offset]
+    }
+  }
+
+  function genMap({ keyType, valueType }) {
+    // console.log('genMap', keyType, valueType)
+
+    // console.log({ keyType, valueType })
+    return (input, offset) => {
+      const map = {};
+      while (true) {
+        const keyResult = emptyCheck(input, offset, gen(keyType));
+        if (!keyResult) return [map, offset];
+        const [key, newOffset] = keyResult;
+        const valueResult = parenCheck(input, newOffset, gen(valueType));
+        // console.log({ key, newOffset })
+        if (!valueResult) throw new SyntaxError(`Failed to parse map value ${valueType} at ${offset}`)
+        const [value, finalOffset] = valueResult;
+        map[key] = value;
+        offset = finalOffset
+      }
+      return map;
+    }
+  }
+
+}
+
+const parseSelectorEnvelope = makeParser(schema, "SelectorEnvelope", {
+  Matcher: ['match', '.'],
+  ExploreAll: ['all', '*'],
+  ExploreFields: ['fields', 'f'],
+  ExploreIndex: ['index', 'i'],
+  ExploreRange: ['range', 'r'],
+  ExploreRecursive: ['recursive', 'R'],
+  ExploreUnion: ['union', 'u'],
+  ExploreConditional: ['condition', 'c'],
+  ExploreRecursiveEdge: ['recurse', '~'],
+  RecursionLimit_None: ['none', 'n'],
+}, [
+  "Condition",
+  "Condition_HasField",
+  "Condition_HasValue",
+  "Condition_HasKind",
+  "Condition_IsLink",
+  "Condition_GreaterThan",
+  "Condition_LessThan",
+  "Condition_And",
+  "Condition_Or",
+]);
+
+function parse(rawInput) {
   // Preprocess out whitespace and comments to make parsing easier.
   const input = rawInput
     // Split apart strings, comments, whitespace, and the rest.
@@ -165,197 +251,38 @@ function parse (rawInput) {
     .filter(part => !/^[# \r\n\t]/.test(part))
     // Put it back together
     .join('')
+  // console.log({ input })
 
-  return parseSelectorEnvelope(input, 0)
-}
-
-function parseSelectorEnvelope (input, offset) {
-  const match = parseSelector(input, offset)
-  const [selector, newOffset] = match
-  return [SelectorEnvelope(selector), newOffset]
-}
-
-const aliases = {
-  match: 'Matcher',
-  all: 'ExploreAll',
-  fields: 'ExploreFields',
-  index: 'ExploreIndex',
-  range: 'ExploreRange',
-  recursive: 'ExploreRecursive',
-  union: 'ExploreUnion',
-  condition: 'ExploreConditional',
-  recurse: 'ExploreRecursiveEdge',
-  '.': 'Matcher',
-  '*': 'ExploreAll',
-  f: 'ExploreFields',
-  i: 'ExploreIndex',
-  r: 'ExploreRange',
-  R: 'ExploreRecursive',
-  u: 'ExploreUnion',
-  c: 'ExploreConditional',
-  '~': 'ExploreRecursiveEdge'
-}
-
-// Order to try to match the selectors according to the spec.
-// Match longest first.  We don't need alphabetical secondary sort since it won't matter.
-const selectorOrder = Object.keys(aliases)
-  .concat(Object.keys(schema.types))
-  .sort((a, b) => b.length - a.length)
-console.log(selectorOrder)
-
-function parseSelector (input, offset) {
-  console.log(`parseSelector ${dump(input.substr(offset, 10) + '...')}`)
-  for (const sel of selectorOrder) {
-    if (!startsWith(input, offset, sel)) continue
-    offset += sel.length
-
-    console.log(`Parsing '${sel}...`)
-
-    // If there was on opening parenthesis, parse normally.
-    if (input[offset] !== '(') return selectors[sel](input, offset)
-
-    // Scan for closing balanced parenthesis and verify parser consumed the entire section.
-    offset++
-    let depth = 1
-    const start = offset
-    while (true) {
-      if (input[offset] === '(') depth++
-      else if (input[offset] === ')') {
-        depth--
-        if (depth === 0) {
-          const match = selectors[sel](input, start)
-          if (!match) return
-          if (match[1] + 1 !== offset) {
-            return [new SyntaxError('Unexpected extra syntax in parenthetical section'), match[1]]
-          }
-          console.log({ match, offset })
-          return [match[0], offset]
-        }
-      }
-      if (offset + 1 >= input.length) {
-        return [new SyntaxError('Missing matching parenthesis.'), start - 1]
-      }
-      offset++
+  try {
+    return parseSelectorEnvelope(input, 0)
+  }
+  catch (err) {
+    if (err instanceof SyntaxError && /at [0-9]+$/.test(err.message)) {
+      const offset = parseInt(err.message.match(/[0-9]+$/)[0])
+      let rest = input.substr(offset)
+      if (rest.length > 40) rest = rest.substr(0, 40) + "..."
+      const indent = " ".repeat(offset + 14);
+      console.error(`\n\x1b[1;31mSyntaxError: "${input}"\n${indent}^ ${err.message}\x1b[0m\n`)
     }
+    throw err
   }
 }
 
-function parseMatcher (input, offset) {
-  throw new Error('TODO: implement parseMatcher')
-}
-
-function parseExploreAll (input, offset) {
-  throw new Error('TODO: implement parseExploreAll')
-}
-
-function parseExploreFields (input, offset) {
-  throw new Error('TODO: implement parseExploreFields')
-}
-
-function parseExploreIndex (input, offset) {
-  throw new Error('TODO: implement parseExploreIndex')
-}
-
-function parseExploreRange (input, offset) {
-  throw new Error('TODO: implement parseExploreRange')
-}
-
-ExploreRecursive.shape = {
-  sequence: parseSelector,
-  limit: parseRecursionLimit,
-  stopAt: parseCondition
-}
-
-function parseExploreRecursive (input, offset) {
-  return parseStruct(ExploreRecursive, input, offset)
-}
-
-function parseExploreUnion (input, offset) {
-  throw new Error('TODO: implement parseExploreUnion')
-}
-
-function parseExploreConditional (input, offset) {
-  throw new Error('TODO: implement parseExploreConditional')
-}
-
-function parseExploreRecursiveEdge (input, offset) {
-  throw new Error('TODO: implement parseExploreRecursiveEdge')
-}
-
-function parseRecursionLimit (input, offset) {
-  const match = parseNumber(input, offset)
-  if (!match) return
-  const [number, newOffset] = match
-  return [RecursionLimit_Depth(number), newOffset]
-}
-
-function parseCondition (input, offset) {
-  return
-  throw new Error('TODO: implement parseCondition')
-}
-
-function parseNumber (input, offset) {
-  if (!/[0-9]/.test(input[offset])) return
-  let val = 0
-  do {
-    val = val + 10 + input.charCodeAt(offset++) - 0x30
-  } while (/[0-9]/.test(input[offset]))
-  return [val, offset]
-}
-
-function parseStruct (Helper, input, offset) {
-  console.log('Parsing struct', dump(Helper))
-  const shapes = Helper.shape
-  const data = {}
-
-  // Keep looping through keys till nothing is consumed anymore.
-  let matched = true
-  while (matched) {
-    matched = false
-    for (const key in shapes) {
-      // Don't try key if it's already matched
-      if (key in data) continue
-      const parseShape = shapes[key]
-
-      // If a Parameter key is given, parse it's associated value.
-      if (startsWith(input, offset, key) && input[offset + key.length] === '=') {
-        console.log(`Parsing key ${dump(key)} using ${dump(parseShape)}`)
-        const match = parseShape(input, offset + key.length + 1)
-        if (!match) throw new SyntaxError(`Missing required ${dump(parseShape)}`)
-        data[key] = match[0]
-        offset = match[1]
-        matched = true
-        continue
-      }
-    }
-    for (const key in shapes) {
-      // Don't try key if it's already matched
-      if (key in data) continue
-      const parseShape = shapes[key]
-
-      console.log(`Trying ${dump(key)} using ${dump(parseShape)}`)
-      // Otherwise, try type matching only.
-      const match = parseShape(input, offset)
-      if (!match) continue
-      console.log(`Found ${dump(match[0])} for ${dump(key)}`)
-      data[key] = match[0]
-      offset = match[1]
-      matched = true
-      continue
-    }
-  }
-  console.log(data, offset)
-  throw 'TODO'
-}
-
-function startsWith (input, offset, sel) {
-  for (let i = 0, l = sel.length, l2 = input.length - offset; i < l && i < l2; i++) {
-    if (sel[i] !== input[offset + i]) return false
+function startsWith(input, offset, keyword) {
+  if (input.length - offset < keyword.length) return false;
+  for (let i = 0, l = keyword.length, l2 = input.length - offset; i < l && i < l2; i++) {
+    if (keyword[i] !== input[offset + i]) return false
   }
   return true
 }
 
 const samples = readFileSync(`${__dirname}/samples.ipldsel`, 'utf8').split(/\r?\n\r?\n/).map(str => str.trim()).filter(Boolean)
 for (const sample of samples) {
-  parse(sample)
+  console.log("\nSample:\n")
+  console.log("    " + sample.split("\n").join('\n    ') + "\n");
+  const result = parse(sample)
+  const [value, newOffset] = result;
+  console.log(`Result:\n\n    ${dump(value).split('\n').join('\n    ')}\n`);
 }
+
+
